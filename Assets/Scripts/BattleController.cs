1e38c158f61dfe563f79cb3d435a0afde62eb20d
misoshiruseijin <ayano.hiranaka@gmail.com>
2021-01-21 15:01:42 -0500

    First commit. Project and .gitignore

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+    public Character player;
+    public Character enemy;
+    public GameObject ResultPanel;
+
+    private bool IsPlayerTurn;
+    private bool IsGameOver;
+    private float time = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsPlayerTurn = true;
+        IsGameOver = false;
+        ResultPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGameOver)
+        {
+            ShowResultPanel();
+            return;
+        }
+
+        if (!IsPlayerTurn)
+        {
+            time += Time.deltaTime;
+            if(time >= 1f)
+            {
+                player.ReceiveDamage(enemy.atk);
+                IsPlayerTurn = true;
+                time = 0f;
+            }
+        }
+
+        if (player.hp == 0 || enemy.hp == 0)
+        {
+            IsGameOver = true;
+        }
+    }
+
+    public void PushAttackButton()
+    {
+        enemy.ReceiveDamage(player.atk);
+        IsPlayerTurn = false;
+    }
+
+    void ShowResultPanel()
+    {
+        ResultPanel.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public Slider hpSlider;
+
+    public int hp;
+    public int Maxhp = 100;
+    public int atk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = Maxhp;
+        hpSlider.maxValue = Maxhp;
+        hpSlider.value = Maxhp;
+        atk = 10;
+    }
+
+    public void ReceiveDamage(int _damage)
+    {
+        hp -= _damage;
+        hpSlider.value = hp;
+        if (hp < 0)
+        {
+            hpSlider.value = 0;
+        }
+    }
+
+}
