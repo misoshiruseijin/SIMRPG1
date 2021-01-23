835ecc2d223c9583ad3155d4e86ce8e24f69075e
misoshiruseijin <ayano.hiranaka@gmail.com>
2021-01-23 17:24:30 -0500

    Effect played with attack

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,23 +6,31 @@ using UnityEngine.UI;
 public class Character : MonoBehaviour
 {
     public Slider hpSlider;
+    public GameObject explosionPrefab;
+    public GameObject hpText;
 
     public int hp;
     public int Maxhp = 100;
     public int atk;
 
+
     // Start is called before the first frame update
     void Start()
     {
         hp = Maxhp;
+        hpText.GetComponent<Text>().text = "HP: " + hp.ToString();
+        atk = 10;
+
         hpSlider.maxValue = Maxhp;
         hpSlider.value = Maxhp;
-        atk = 10;
     }
 
     public void ReceiveDamage(int _damage)
     {
+        GameObject tempParticle = Instantiate(explosionPrefab, this.transform.position, Quaternion.identity) as GameObject;
+
         hp -= _damage;
+        hpText.GetComponent<Text>().text = "HP: " + hp.ToString();
         hpSlider.value = hp;
         if (hp < 0)
         {
---
+++ Assets/Scripts/ParticleObjDelete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleObjDelete : MonoBehaviour
+{
+    ParticleSystem particle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        StartCoroutine(DeleteCoroutine());
+    }
+
+    IEnumerator DeleteCoroutine()
+    {
+        yield return new WaitWhile(() => particle.IsAlive(true));
+        Destroy(this.gameObject);
+    }
+}
