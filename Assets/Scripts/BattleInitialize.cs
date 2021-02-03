dbf30033a260f9d3e33a661aa18fb326306854e9
misoshiruseijin <ayano.hiranaka@gmail.com>
2021-02-03 16:35:46 -0500

    started battle initializer

---
+++ Assets/Scripts/BattleInitialize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BattleInitialize
+{
+    ///<summary>
+    ///戦闘に参加するユニットの情報を受け取る。
+    ///CSVReaderから受け取ったデータから必要なデータだけをまとめてBattleControllerに渡す
+    ///</summary>
+    
+    public static void GetPlayerUnitData()
+    {
+
+    }
+
+    public static void GetEnemyData()
+    {
+
+    }
+
+}
