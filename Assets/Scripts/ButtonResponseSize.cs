78315357fcb504fe5151c9396316e963aa198f19
misoshiruseijin <ayano.hiranaka@gmail.com>
2021-04-15 18:07:33 -0400

    started simulation scene UI

---
+++ Assets/Scripts/ButtonResponseSize.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonResponseSize : MonoBehaviour
+{
+    public bool btnReady;
+    
+    private float speed = 2f;
+    private float scale; 
+    private float maxScale = 1.15f; // 最大倍率
+    private float minScale = 1.0f; // 最小倍率
+    private float time;
+    private Image image;
+    private float width, height;
+    private RectTransform rt;
+
+    void Start()
+    {
+        btnReady = false;
+        image = this.GetComponent<Image>();
+        rt = this.GetComponent<RectTransform>();
+        width = rt.rect.width;
+        height = rt.rect.height;
+    }
+
+    void Update()
+    {
+        if (!btnReady)
+        {
+            // 通常状態
+            image.rectTransform.sizeDelta = new Vector2(width, height);
+            time = 0f;
+        }
+
+        else
+        {
+            // サイズを変える
+            scale = GetImageScale();
+            image.rectTransform.sizeDelta = new Vector2(scale * width, scale * height);
+        }
+    }
+
+    private float GetImageScale()
+    {
+        time += Time.deltaTime;
+        scale = (0.5f * (maxScale - minScale)) * Mathf.Sin(speed * time) + (0.5f * (maxScale - minScale)) + minScale;
+
+        return scale;
+    }
+}
---
+++ Assets/Scripts/SimulationMenu.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SimulationMenu : MonoBehaviour
+{
+    public List<GameObject> btnList;
+
+    private EventSystem eventSystem;
+    private GameObject button, prevButton;
+    private ButtonResponseSize btnResp;
+    private int btnID, prevBtnID;
+
+    public void ButtonPressed()
+    {
+        eventSystem = EventSystem.current;
+        prevButton = button;
+        button = eventSystem.currentSelectedGameObject;
+        btnResp = button.GetComponent<ButtonResponseSize>();
+
+        prevBtnID = btnID;
+        btnID = btnList.IndexOf(button);
+
+        bool btnChanged = btnID != prevBtnID; // 前と違うボタンを押した
+        bool noPrevBtn = prevButton == null; // 前に押したボタンがない (今回初めてボタンを押した)
+
+        //Debug.Log(button);
+
+        // 同じパネル内で前と違うボタンを押した
+        if (!noPrevBtn && btnChanged)
+        {
+            //Debug.Log(noPrevBtn + ", " + btnChanged);
+            prevButton.GetComponent<ButtonResponseSize>().btnReady = false;
+        }
+
+        if (!btnResp.btnReady)
+        {
+            //Debug.Log("初めてクリックされた");
+            btnResp.btnReady = true;
+        }
+
+        else if (btnResp.btnReady)
+        {
+            //Debug.Log("二回目のクリック");
+            btnResp.btnReady = false;
+        }
+    }
+
+    private void TakeButtonAction(int btnID)
+    {
+        switch (btnID)
+        {
+            case 0:
+                //Debug.Log("仲間管理ボタン");
+                break;
+
+            case 1:
+                //Debug.Log("会話ボタン");
+                break;
+
+            case 2:
+                //Debug.Log("マップボタン");
+                break;
+
+            case 3:
+                //Debug.Log("設定ボタン");
+                break;
+
+            case 4:
+                //Debug.Log("戦闘開始ボタン");
+                break;
+        }
+    }
+}
