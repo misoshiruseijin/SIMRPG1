42b8620ad1e29738be5c348935f4bb67ce864d59
misoshiruseijin <ayano.hiranaka@gmail.com>
2021-05-19 17:18:16 -0400

    revert

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Animator newDayTransition;
+    public float transitionTime = 1f;
+
+    private string simulation = "SimulationScene";
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NewDay();
+        }
+    }
+
+    public void NewDay()
+    {
+        StartCoroutine("LoadNewDay");
+    }
+
+    IEnumerator LoadNewDay()
+    {
+        newDayTransition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(simulation);
+    }
+}
