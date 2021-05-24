873f4b214efd596a204b1cf371f43ea17e52cfd2
misoshiruseijin <ayano.hiranaka@gmail.com>
2021-05-24 17:16:25 -0400

    changed code to generate toggles each time menu is opened (fixed toggle state and update status issues); evolve takes multiple days

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,6 +14,11 @@ public class GameController: MonoBehaviour
     public List<CharacterData> partyMemberData; // 戦闘に参加するユニットの情報
     public List<GeneData> geneDataList; // 所持している遺伝子アイテムのリスト
 
+    public int daysFromEvolve; // 変異開始から経過した日数
+    public int evolveDays; // 変異完了までに必要な日数
+    public int evolvingUnitID; // 変異中のキャラID
+    public int usedGeneID; // 変異中のキャラに使用された遺伝子ID
+    public bool isEvolving; // 変異中のキャラがいるか
     public int phase; // ゲームの進行状況
     public float messaegSpeed; // 文字表示速度
     public int day; // 開始からの日数
--- Assets/Scripts/SimulationMenu.cs
+++ Assets/Scripts/SimulationMenu.cs
@@ -39,6 +39,9 @@ public class SimulationMenu : MonoBehaviour
     public List<GeneData> geneDataList; // 所持している遺伝子アイテムのリスト
 
     private int day, food, survivors, phase;
+    private int evolveDays; // 変異完了までにかかる日数
+    private int evolvingUnitID; // 変異中のユニットのID (evolveUnitIDはAllyMenuが更新されると更新される。evolvingUnitIDは次の変異を開始するまで変化しない)
+    private bool isEvolving; // 変異中フラグ
 
     private EventSystem eventSystem;
     private List<GameObject> btnList;
@@ -52,7 +55,6 @@ public class SimulationMenu : MonoBehaviour
     private int evolveUnitID, useGeneID; // 育成されるユニットID、使用する遺伝子アイテムID
     private List<int> partyMemberID;
     private GameObject skillDescObj;
-    private bool updateStatusFlg, updateEvolveMenuFlg;
     private DialogBox dialog;
     #endregion
     #endregion
@@ -93,8 +95,6 @@ public class SimulationMenu : MonoBehaviour
         partyToggleGroup = partyMenuToggleParent.GetComponent<ToggleGroup>();
         evolveToggleGroup = evolveMenuToggleParent.GetComponent<ToggleGroup>();
         skillDescObj = allyStatusPanel.transform.Find("SkillDescText").gameObject;
-        updateStatusFlg = false;
-        updateEvolveMenuFlg = false;
         dialog = DialogBox.Instance();
 
         activeToggleID = -1; // default value
@@ -114,30 +114,13 @@ public class SimulationMenu : MonoBehaviour
         survivorText.text = $"X {survivors}";
         dateText.text = $"{day} 日目";
 
-        // 味方管理画面の設定
-        // トグルオブジェクトを作成 (初期状態では全部オフ)
-        allyMenuToggleList = ToggleListFromAllyData(allyMenuToggleParent, allyToggleGroup);
-        foreach (GameObject toggleObj in allyMenuToggleList)
-        {
-            toggleObj.GetComponent<Toggle>().onValueChanged.AddListener((bool value) => AllyMenuToggleStateChange());
-        }
+        isEvolving = GameController.instance.isEvolving;
 
-        // パーティー編成画面の設定
-        // トグルオブジェクトを作成 (初期状態では全部オフ)
-        partyMenuToggleList = ToggleListFromAllyData(partyMenuToggleParent, partyToggleGroup);
-        foreach (GameObject toggleObj in partyMenuToggleList)
+        // 変異中なら、変異が完了したか確認
+        if (isEvolving && GameController.instance.daysFromEvolve >= GameController.instance.evolveDays)
         {
-            toggleObj.GetComponent<Toggle>().onValueChanged.AddListener((bool value) => PartyMenuToggleStateChange());
+            EvolveComplete();
         }
-
-        // 育成画面の設定
-        // トグルオブジェクトを作成 (初期状態では全部オフ)
-        evolveMenuToggleList = ToggleListFromGeneData(evolveMenuToggleParent, evolveToggleGroup);
-        foreach (GameObject toggleObj in evolveMenuToggleList)
-        {
-            toggleObj.GetComponent<Toggle>().onValueChanged.AddListener((bool value) => EvolveMenuToggleStateChange());
-        }
-
     }
 
     public void ButtonPressed()
@@ -182,40 +165,66 @@ public class SimulationMenu : MonoBehaviour
         prevButton = null;
         button = null;
 
+        // NEW // 
+        // いったん全部非表示
         if (btnID < nMainBtns)
         {
-            //Debug.Log("メインメニューのボタンが押された。対応したパネルを表示する");
             for (int i = 0; i < nPanels; i++)
             {
-                if (i == btnID)
-                {
-                    PanelController.EnablePanel(popupPanelList[i]);
-                }
-
-                else
-                {
-                    PanelController.DisablePanel(popupPanelList[i]);
-                }
+                PanelController.DisablePanel(popupPanelList[i]);
             }
         }
+        PanelController.EnablePanel(popupPanelList[btnID]);
 
-        else
+        switch (btnID)
         {
-            //Debug.Log("メインメニュー以外のボタンが押された。メインパネルに重ねてパネルを表示する");
-            PanelController.EnablePanel(popupPanelList[btnID]);
-        }
+            case 0:
+                // 味方管理画面へ
+                // トグルオブジェクトを作成 (初期状態では全部オフ)
+                foreach(GameObject toggleObj in allyMenuToggleList)
+                {
+                    Destroy(toggleObj, 0f);
+                }
+                allyMenuToggleList = ToggleListFromAllyData(allyMenuToggleParent, allyToggleGroup);
+                foreach (GameObject toggleObj in allyMenuToggleList)
+                {
+                    toggleObj.GetComponent<Toggle>().onValueChanged.AddListener((bool value) => AllyMenuToggleStateChange());
+                }
+                break;
 
-        if (updateStatusFlg)
-        {
-            // キャラを進化させていた場合、次にメニューを開いたときステータスがアップデートされるようにする
-            allyMenuToggleList[0].GetComponent<Toggle>().isOn = true;
-            partyMenuToggleList[0].GetComponent<Toggle>().isOn = true;
-            activeToggleID = 0;
-            UpdateAllyMenu();
-            UpdatePartyMenu();
-            activeToggleID = -1;
-            updateStatusFlg = false;
+            case 1:
+                // パーティー編成へ
+                // トグルオブジェクトを作成 (初期状態では全部オフ)
+                foreach (GameObject toggleObj in partyMenuToggleList)
+                {
+                    Destroy(toggleObj, 0f);
+                }
+                partyMenuToggleList = ToggleListFromAllyData(partyMenuToggleParent, partyToggleGroup);
+                foreach (GameObject toggleObj in partyMenuToggleList)
+                {
+                    toggleObj.GetComponent<Toggle>().onValueChanged.AddListener((bool value) => PartyMenuToggleStateChange());
+                }
+                break;
+
+            case 5:
+                // 変異画面を重ねて表示
+                // トグルオブジェクトを作成 (初期状態では全部オフ)
+                foreach (GameObject toggleObj in evolveMenuToggleList)
+                {
+                    Destroy(toggleObj, 0f);
+                }
+                evolveMenuToggleList = ToggleListFromGeneData(evolveMenuToggleParent, evolveToggleGroup);
+                foreach (GameObject toggleObj in evolveMenuToggleList)
+                {
+                    toggleObj.GetComponent<Toggle>().onValueChanged.AddListener((bool value) => EvolveMenuToggleStateChange());
+                }
+                break;
+            
+            default:
+                Debug.Log("会話、マップ、設定のどれかを表示");
+                break;
         }
+        // NEW END // 
     }
 
     public void AllyMenuToggleStateChange()
@@ -235,39 +244,6 @@ public class SimulationMenu : MonoBehaviour
             UpdateAllyMenu();
         }
     }
-
-    //public void PartyMenuToggleStateChange()
-    //{
-    //    //Debug.Log("Party Menu Toggle State Changed");
-    //    GameObject activeToggle = partyToggleGroup.ActiveToggles().FirstOrDefault().gameObject;
-    //    GameObject paramObj, skillObj;
-
-    //    paramObj = partyStatusPanel.transform.Find("UnitParam").transform.Find("UnitParamValue").gameObject;
-    //    skillObj = partyStatusPanel.transform.Find("UnitSkillText").gameObject;
-
-    //    prevToggleID = activeToggleID;
-    //    activeToggleID = partyMenuToggleList.IndexOf(activeToggle);
-
-    //    bool toggleChanged = prevToggleID != activeToggleID;
-
-    //    if (toggleChanged)
-    //    {
-    //        // ステータスパネルを設定
-    //        //Debug.Log("Toggle changed from " + prevToggleID + " to " + activeToggleID);
-
-    //        List<int> statusList = new List<int> { allyDataList[activeToggleID].Maxhp, allyDataList[activeToggleID].atk, allyDataList[activeToggleID].def, allyDataList[activeToggleID].spd };
-    //        paramObj.GetComponent<Text>().text = "\n" + string.Join("\n", statusList.ConvertAll<string>(x => x.ToString()));
-
-    //        List<string> skillNames = new List<string>() { "特殊技能" };
-
-    //        foreach (SkillStatus skill in allyDataList[activeToggleID].skillList)
-    //        {
-    //            skillNames.Add(skill.jpName);
-    //        }
-
-    //        skillObj.GetComponent<Text>().text = string.Join("\n", skillNames.ToArray());
-    //    }
-    //}
 
     public void PartyMenuToggleStateChange()
     {
@@ -299,41 +275,6 @@ public class SimulationMenu : MonoBehaviour
         }
     }
 
-    //public void EvolveMenuToggleStateChange()
-    //{
-    //    //Debug.Log("Evolve Menu Toggle State Change");
-    //    GameObject activeToggle = evolveToggleGroup.ActiveToggles().FirstOrDefault().gameObject;
-
-    //    prevToggleID = activeToggleID;
-    //    activeToggleID = evolveMenuToggleList.IndexOf(activeToggle);
-    //    bool toggleChanged = prevToggleID != activeToggleID;
-
-    //    GameObject paramChange, newSkillText, riskText;
-
-    //    paramChange = evolveStatusPanel.transform.Find("UnitParam").transform.Find("ChangeValue").gameObject;
-    //    newSkillText = evolveStatusPanel.transform.Find("NewSkillText").gameObject;
-    //    riskText = evolveStatusPanel.transform.Find("RiskText").gameObject;
-
-
-    //    useGeneID = activeToggleID;
-
-    //    GeneData activeGeneData = geneDataList[activeToggleID];
-
-    //    if (toggleChanged)
-    //    {
-    //        // ステータスパネルを設定
-    //        List<int> statusList = new List<int> { activeGeneData.hp, activeGeneData.atk, activeGeneData.def, activeGeneData.spd };
-    //        paramChange.GetComponent<Text>().text = "\n" + string.Join("\n", statusList.ConvertAll<string>(x => x.ToString()));
-
-    //        // 追加スキルの表記を更新
-    //        newSkillText.GetComponent<Text>().text = "追加される能力\n" + activeGeneData.skill.jpName + "\n" + activeGeneData.skill.desc;
-
-    //        // リスクレベルを更新
-    //        (string riskString, Color riskTextColor) = RiskLevelString(activeGeneData.risk);
-    //        riskText.GetComponent<Text>().text = riskString;
-    //        riskText.GetComponent<Text>().color = riskTextColor;
-    //    }
-    //}
     public void CloseButtonPressed()
     {
         // 閉じるボタンを押したら対応したパネルを閉じる。閉じるボタンはパネルの子オブジェクトでなくてはいけない
@@ -458,66 +399,73 @@ public class SimulationMenu : MonoBehaviour
         skillTextObj.GetComponent<Text>().text = "特殊能力\n" + string.Join("\n", skillNameList);
 
         TakeButtonAction(btnID);
-
-        if (updateEvolveMenuFlg)
-        {
-            evolveMenuToggleList[0].GetComponent<Toggle>().isOn = true;
-        }
-
     }
 
     public void StartEvolveButtonPressed()
     {
         Debug.Log("変異開始ボタンが押された");
         CloseAllPanels();
-        // 代わりに進化演出　→　新ステータスの表示
+        // 代わりに進化演出を入れる
 
-        CharacterData newData = allyDataList[evolveUnitID];
-        GeneData gene = geneDataList[useGeneID];
-        int riskPercent = gene.risk;
-
-        // ステータス変化を反映
-        if (riskPercent < 10)
-        {
-            // リスク値10未満ならランダム要素なし
-            newData.Maxhp += gene.hp;
-            newData.atk += gene.atk;
-            newData.def += gene.def;
-            newData.spd += gene.spd;
-        }
-
-        else
-        {
-            // ランダム要素あり。定数を計算
-            int[] paramArray = new int[] { newData.Maxhp, newData.atk, newData.def, newData.spd };
-            int[] growthArray = new int[] { gene.hp, gene.atk, gene.def, gene.spd };
-            for (int i = 0; i < growthArray.Length; i++)
-            {
-                int factor = ProbabilityCalculator.GrowthFactor(riskPercent);
-                //Debug.Log("Factor" + i + ": " + factor);
-                paramArray[i] = Mathf.RoundToInt((1 + (factor / 100f)) * paramArray[i]);
-            }
-
-            newData.Maxhp = paramArray[0];
-            newData.atk = paramArray[1];
-            newData.def = paramArray[2];
-            newData.spd = paramArray[3];
-        }
-        
-        newData.skillList.Add(gene.skill);
-
-        allyDataList[evolveUnitID] = newData; // 進化前の情報を上書きする
-        geneDataList.RemoveAt(useGeneID); // アイテムリストから消費したアイテムを削除
-        Destroy(evolveMenuToggleList[useGeneID]); // 消費されたアイテムのトグルを削除
-        evolveMenuToggleList.RemoveAt(useGeneID); // トグルリストからトグルを削除
-
-        if (evolveMenuToggleList.Count > 0)
-        {
-            updateEvolveMenuFlg = true;
-        }
+        // NEW //
+        evolvingUnitID = evolveUnitID;
+        GameController.instance.daysFromEvolve = 0;
+        evolveDays = 2; // 本来はgenedataから取得
+        isEvolving = true;
+        GameController.instance.usedGeneID = useGeneID;
+        GameController.instance.evolveDays = evolveDays;
 
         activeToggleID = -1; // 初期化
-        updateStatusFlg = true;
+        // NEW END //
+
+        #region 原文
+        //CharacterData newData = allyDataList[evolveUnitID];
+        //GeneData gene = geneDataList[useGeneID];
+        //int riskPercent = gene.risk;
+
+        //// ステータス変化を反映
+        //if (riskPercent < 10)
+        //{
+        //    // リスク値10未満ならランダム要素なし
+        //    newData.Maxhp += gene.hp;
+        //    newData.atk += gene.atk;
+        //    newData.def += gene.def;
+        //    newData.spd += gene.spd;
+        //}
+
+        //else
+        //{
+        //    // ランダム要素あり。定数を計算
+        //    int[] paramArray = new int[] { newData.Maxhp, newData.atk, newData.def, newData.spd };
+        //    int[] growthArray = new int[] { gene.hp, gene.atk, gene.def, gene.spd };
+        //    for (int i = 0; i < growthArray.Length; i++)
+        //    {
+        //        int factor = ProbabilityCalculator.GrowthFactor(riskPercent);
+        //        //Debug.Log("Factor" + i + ": " + factor);
+        //        paramArray[i] = Mathf.RoundToInt((1 + (factor / 100f)) * paramArray[i]);
+        //    }
+
+        //    newData.Maxhp = paramArray[0];
+        //    newData.atk = paramArray[1];
+        //    newData.def = paramArray[2];
+        //    newData.spd = paramArray[3];
+        //}
+
+        //newData.skillList.Add(gene.skill);
+
+        //allyDataList[evolveUnitID] = newData; // 進化前の情報を上書きする
+        //geneDataList.RemoveAt(useGeneID); // アイテムリストから消費したアイテムを削除
+        //Destroy(evolveMenuToggleList[useGeneID]); // 消費されたアイテムのトグルを削除
+        //evolveMenuToggleList.RemoveAt(useGeneID); // トグルリストからトグルを削除
+
+        //if (evolveMenuToggleList.Count > 0)
+        //{
+        //    updateEvolveMenuFlg = true;
+        //}
+
+        //activeToggleID = -1; // 初期化
+        //updateStatusFlg = true;
+        #endregion
     }
 
     public void NextDayButtonPressed()
@@ -590,7 +538,9 @@ public class SimulationMenu : MonoBehaviour
     {
         //Debug.Log("UpdateAllyMenuCalled");
         GameObject unitName, unitImage, unitParam, unitDesc;
+        Button evolveBtn;
 
+        evolveBtn = allyStatusPanel.transform.Find("EvolveButton").gameObject.GetComponent<Button>(); // NEW
         unitName = allyStatusPanel.transform.Find("UnitNameText").gameObject;
         unitImage = allyStatusPanel.transform.Find("UnitImage").gameObject;
         unitParam = allyStatusPanel.transform.Find("UnitParam").transform.Find("UnitParamValue").gameObject;
@@ -618,7 +568,12 @@ public class SimulationMenu : MonoBehaviour
         skillBtnList = GenerateSkillBtnList(skillNames);
 
         unitDesc.GetComponent<Text>().text = "キャラ説明文";
-        
+
+        // 変異中のキャラがいる場合、変異ボタンをロック
+        if (isEvolving && activeToggleID == evolvingUnitID)
+        {
+            evolveBtn.interactable = false;
+        }        
     }
 
     private void UpdatePartyMenu()
@@ -727,6 +682,7 @@ public class SimulationMenu : MonoBehaviour
         ManageData.SaveGeneData(geneDataList);
         day++;
 
+        // 食料の残量更新
         if (food < survivors + allyDataList.Count)
         {
             food = 0;
@@ -736,10 +692,68 @@ public class SimulationMenu : MonoBehaviour
             food -= (survivors + allyDataList.Count);
         }
 
+        // 変異中の場合
+        if (isEvolving)
+        {
+            GameController.instance.daysFromEvolve++;
+        }
+
+        // GameControllerに保存
         GameController.instance.day = day;
         GameController.instance.food = food;
+        GameController.instance.isEvolving = isEvolving;
 
         sceneChanger.NewDay();
+    }
+
+    private void EvolveComplete()
+    {
+        Debug.Log("変異が完了した");
+
+        int geneID = GameController.instance.usedGeneID;
+        isEvolving = false;
+        GameController.instance.daysFromEvolve = 0;
+
+        CharacterData newData = allyDataList[evolvingUnitID];
+        GeneData gene = geneDataList[geneID];
+        int riskPercent = gene.risk;
+
+        // ステータス変化を反映
+        if (riskPercent < 10)
+        {
+            // リスク値10未満ならランダム要素なし
+            newData.Maxhp += gene.hp;
+            newData.atk += gene.atk;
+            newData.def += gene.def;
+            newData.spd += gene.spd;
+        }
+
+        else
+        {
+            // ランダム要素あり。定数を計算
+            int[] paramArray = new int[] { newData.Maxhp, newData.atk, newData.def, newData.spd };
+            int[] growthArray = new int[] { gene.hp, gene.atk, gene.def, gene.spd };
+            for (int i = 0; i < growthArray.Length; i++)
+            {
+                int factor = ProbabilityCalculator.GrowthFactor(riskPercent);
+                //Debug.Log("Factor" + i + ": " + factor);
+                paramArray[i] = Mathf.RoundToInt((1 + (factor / 100f)) * paramArray[i]);
+            }
+
+            newData.Maxhp = paramArray[0];
+            newData.atk = paramArray[1];
+            newData.def = paramArray[2];
+            newData.spd = paramArray[3];
+        }
+
+        newData.skillList.Add(gene.skill);
+
+        allyDataList[evolveUnitID] = newData; // 進化前の情報を上書きする
+        geneDataList.RemoveAt(geneID); // アイテムリストから消費したアイテムを削除
+        //Destroy(evolveMenuToggleList[geneID]); // 消費されたアイテムのトグルを削除
+        //evolveMenuToggleList.RemoveAt(geneID); // トグルリストからトグルを削除
+
+        activeToggleID = -1; // 初期化
     }
 
     public void TestOnClick()
